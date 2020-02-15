87f6bfbf5a3ad7a0bc48a6c06ad7d334b7ccc2a8
cnix3297 <cnixon3297@gmail.com>
2020-02-15 12:06:28 -0500

    Changed the color variable to a boolean value isWhite. Emplemented the to fenString, and recieving fenString. Made some changes to the Pieces Children to make it a more standardized constructor.

--- Board/src/Board.java
+++ Board/src/Board.java
@@ -3,14 +3,82 @@ public class Board {
     int score;
     String fenString;
 
-    public Board(String fen){ // Need to intake fenstring, Current color turn char, Bottom color char(Player Color), and Depth int
+    public Board(String fen) { // Need to intake fenstring, Current color turn char, Bottom color char(Player Color), and Depth int
         // Create board from fenstring;
+        int currentRow = 0;
+        int currentColumn = 0;
+        String[] reg = fen.split(" ");
 
+        //ToDo reg[1] holds the value for who's move it is
+
+        for (int i = 0; i < reg[0].length(); i++) {
+            switch (reg[0].charAt(i)) {
+                case 'P':
+                    board[currentColumn][currentRow] = new PawnPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'p':
+                    board[currentColumn][currentRow] = new PawnPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'R':
+                    board[currentColumn][currentRow] = new RookPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'r':
+                    board[currentColumn][currentRow] = new RookPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'N':
+                    board[currentColumn][currentRow] = new KnightPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'n':
+                    board[currentColumn][currentRow] = new KnightPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'B':
+                    board[currentColumn][currentRow] = new BishopPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'b':
+                    board[currentColumn][currentRow] = new BishopPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'Q':
+                    board[currentColumn][currentRow] = new QueenPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'q':
+                    board[currentColumn][currentRow] = new QueenPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'K':
+                    board[currentColumn][currentRow] = new KingPiece(true, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case 'k':
+                    board[currentColumn][currentRow] = new KingPiece(false, currentRow, currentColumn);
+                    currentColumn++;
+                    break;
+                case '/':
+                    currentRow++;
+                    currentColumn = 0;
+                    break;
+                default:
+                {
+                    String hold = "" + fen.charAt(i);
+                    currentRow = currentRow + Integer.parseInt(hold);
+                }
+            }
+        }
     }
 
     public Board(Board b, BoardPosition oldPos, BoardPosition newPos){ // Incoming board, old position and new position
         // Check if this is an attack and call capture?
     }
+
+
 
     public boolean isPiece(){
         return true;
@@ -18,8 +86,67 @@ public class Board {
 
     private void Capture(){}
 
+    //Lowercase is Black
     public String createFenString(){
-        String fen = "";
-        return fen;
+        StringBuilder fen = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            int count = 0;
+            boolean isEmpty = false;
+            for (int j = 0; j < 8; j++) {
+                if (board[j][i] == null){
+                    isEmpty = true;
+                    count++;
+                    continue;
+                }else if (isEmpty){
+                    fen.append(count);
+                    count = 0;
+                }
+                switch(board[j][i].type){
+                    case "Pawn":
+                        if (board[j][i].isWhite)
+                            fen.append("P");
+                        else
+                            fen.append("p");
+                        break;
+                    case "Rook":
+                        if (board[j][i].isWhite)
+                            fen.append("R");
+                        else
+                            fen.append("r");
+                        break;
+                    case "Knight":
+                        if (board[j][i].isWhite)
+                            fen.append("N");
+                        else
+                            fen.append("n");
+                        break;
+                    case "Bishop":
+                        if (board[j][i].isWhite)
+                            fen.append("B");
+                        else
+                            fen.append("b");
+                        break;
+                    case "Queen":
+                        if (board[j][i].isWhite)
+                            fen.append("Q");
+                        else
+                            fen.append("q");
+                        break;
+                    case "King":
+                        if (board[j][i].isWhite)
+                            fen.append("K");
+                        else
+                            fen.append("k");
+                        break;
+                }
+            }
+            fen.append("/");
+        }
+        fen.append(" " + "w" + "");
+
+        return fen.toString();
     }
+
+
+
 }
--- Board/src/PawnPiece.java
+++ Board/src/PawnPiece.java
@@ -1,7 +1,8 @@
 public class PawnPiece extends Pieces {
-    public PawnPiece(String t, String c, int s, int r, int col) {
-        super(t, c, s, r, col); // Change the move list based on color
+    public PawnPiece(Boolean isWhite, int r, int col) {
+        super("Pawn", isWhite, 1, r, col); // Change the move list based on color
     }
+
 
     // Need to overwrite getPossiblePieces
 }
