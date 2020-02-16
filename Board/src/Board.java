e4be50242230c31eb1d26c7d9512b455074af59f
cnix3297 <cnixon3297@gmail.com>
2020-02-16 09:33:32 -0500

    Emplemented the Capture method for the board class.

--- Board/src/Board.java
+++ Board/src/Board.java
@@ -1,16 +1,25 @@
+import java.util.Hashtable;
+
 public class Board {
     Pieces[][] board = new Pieces[8][8];
     int score;
     String fenString;
+    Hashtable<String,Number> captureOrder = new Hashtable<>();
+
 
     public Board(String fen) { // Need to intake fenstring, Current color turn char, Bottom color char(Player Color), and Depth int
         // Create board from fenstring;
         int currentRow = 0;
         int currentColumn = 0;
         String[] reg = fen.split(" ");
+        captureOrder.put("Pawn", 1);
+        captureOrder.put("Rook",2);
+        captureOrder.put("Bishop",3);
+        captureOrder.put("Knight",4);
+        captureOrder.put("Queen",5);
+
 
         //ToDo reg[1] holds the value for who's move it is
-
         for (int i = 0; i < reg[0].length(); i++) {
             switch (reg[0].charAt(i)) {
                 case 'P':
@@ -84,7 +93,44 @@ public class Board {
         return true;
     }
 
-    private void Capture(){}
+    private <CapturingPiece extends Pieces, CapturedPiece extends Pieces> void Capture(CapturingPiece capturingPiece, CapturedPiece capturedPiece){
+        //ToDo Should we double check to make sure a piece isnt trying to take its own team's piece?
+        //determines the next piece's order
+        int newPieceOrder;
+        if (captureOrder.get(capturedPiece.type).intValue() >= captureOrder.get(capturingPiece.type).intValue() + 3 )
+            newPieceOrder = captureOrder.get(capturedPiece.type).intValue() - 1;
+        else
+            newPieceOrder = captureOrder.get(capturingPiece.type).intValue() + 1;
+        //sets the captureing pieces old board position to null, and the new position is set the the captured pieces old position.
+        switch (newPieceOrder){
+            case 2:
+                //its a rook
+                board[capturedPiece.bp.row][capturedPiece.bp.column] = new RookPiece(capturingPiece.isWhite,capturedPiece.bp.row,capturedPiece.bp.getColumn());
+                board[capturingPiece.bp.row][capturingPiece.bp.column] = null;
+                break;
+            case 3:
+                //its a Bishop
+                board[capturedPiece.bp.row][capturedPiece.bp.column] = new BishopPiece(capturingPiece.isWhite,capturedPiece.bp.row,capturedPiece.bp.getColumn());
+                board[capturingPiece.bp.row][capturingPiece.bp.column] = null;
+                break;
+            case 4:
+                //it's a knight
+                board[capturedPiece.bp.row][capturedPiece.bp.column] = new KnightPiece(capturingPiece.isWhite,capturedPiece.bp.row,capturedPiece.bp.getColumn());
+                board[capturingPiece.bp.row][capturingPiece.bp.column] = null;
+                break;
+            case 5:
+                //its a Queen
+                board[capturedPiece.bp.row][capturedPiece.bp.column] = new QueenPiece(capturingPiece.isWhite,capturedPiece.bp.row,capturedPiece.bp.getColumn());
+                board[capturingPiece.bp.row][capturingPiece.bp.column] = null;
+                break;
+            case 1:
+                //It's a pawn
+                board[capturedPiece.bp.row][capturedPiece.bp.column] = new PawnPiece(capturingPiece.isWhite,capturedPiece.bp.row,capturedPiece.bp.getColumn());
+                board[capturingPiece.bp.row][capturingPiece.bp.column] = null;
+                break;
+        }
+
+    }
 
     //Lowercase is Black
     public String createFenString(){
