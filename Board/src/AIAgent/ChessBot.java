e9839366c1799ef4369d08b9dea0be9501eaffe9
Jason <jdawgerj5@gmail.com>
2020-04-19 21:53:51 -0400

    ChessBot now returns intended results (min and max methods now return heuristic values instead of alpha/beta values)

--- Board/src/AIAgent/ChessBot.java
+++ Board/src/AIAgent/ChessBot.java
@@ -26,23 +26,35 @@ public class ChessBot
 		this.restCallPackage = restCallPackage;
 	}
 
+	// Method is intended to be called from REST endpoint
+	// Will return best move the AI can make in the form of a Board object
 	public Board getBestMoveBoard()
 	{
+		// Board object is obtained from fenString in restCallPackage
 		Board originBoard = new Board(this.restCallPackage.getFenString(), null);
-		System.out.println("0: " + this.restCallPackage.getFenString());
+
+		// System.out.println("0: " + this.restCallPackage.getFenString());
+
+		// Initializes root node of minimax tree
 		BoardNode rootNode = new BoardNode(null, null, 0, 0, originBoard, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+		// Returns bestBoardNode of rootNode as a BoardNode object
 		BoardNode minimaxBoardNode = minimax(rootNode);
+		// Returns the Board object inside of minimaxBoardNode
 		Board resultBoard = minimaxBoardNode.getBoard();
 		return resultBoard;
 	}
 
+	// Method will decide if rootNode is at even or odd depth, before evaluating
 	private BoardNode minimax(BoardNode node)
 	{
+		// If node depth is even, MAXIMIZE
 		if (node.getDepth() % 2 == 0)
 		{
 			int score = maxAlpha(node);
 			return node.getBestBoardNode();
 		}
+		// If node depth is odd, MINIMIZE
 		else
 		{
 			int score = minBeta(node);
@@ -50,15 +62,17 @@ public class ChessBot
 		}
 	}
 
+	// Method will return the most minimized heuristic or return early if pruned
 	private int minBeta(BoardNode node)
 	{
 		int currentDepth = node.getDepth();
 		Board currentBoard = node.getBoard();
 		int maxDepth = this.restCallPackage.getDepth();
-		boolean isAIMoving = currentDepth % 2 == 0;
+		boolean isAIMoving = currentDepth % 2 == 0; // AI only moves on even depths
 		char enemyColor = this.getRestCallPackage().getAiColor() == 'b' ? 'w' : 'b';
 		char movingColor = isAIMoving ? this.getRestCallPackage().getAiColor() : enemyColor;
 
+		// If current depth of node has reached maximum, then terminal node has been reached
 		if (currentDepth == maxDepth)
 		{
 			// Terminal node reached, add score
@@ -66,6 +80,7 @@ public class ChessBot
 			return currentBoard.getAIScore();
 		}
 
+		// Counts how many pieces can be moved on the current player/AI's turn
 		int movingPieceCount = 0;
 		for (Pieces[] row : currentBoard.board)
 		{
@@ -77,6 +92,7 @@ public class ChessBot
 			}
 		}
 
+		// If there are zero pieces that can be moved in current turn then return as a terminal node
 		if (movingPieceCount == 0)
 		{
 			// Terminal node reached, add score
@@ -84,12 +100,18 @@ public class ChessBot
 			return currentBoard.getAIScore();
 		}
 
+		// Set initial value to positive infinity
+		node.setHeuristic(Integer.MAX_VALUE);
+
+		// Search all possible moves
 		for (Pieces[] row : currentBoard.board)
 		{
 			for (Pieces piece : row)
 			{
+				// If piece is an empty square, continue through loop
 				if (piece == null) continue;
 				int possibleMovesCount = piece.getPossibleMoves(currentBoard).size();
+				// If current piece has no moves, continue through loop
 				if (possibleMovesCount == 0)
 				{
 					// Terminal node reached, add score
@@ -98,48 +120,63 @@ public class ChessBot
 					continue;
 				}
 
+				// If piece == movingColor
 				if (piece.getIsWhite() == (movingColor == 'w'))
 				{
 					for (Board currentPossibleMoveBoard : piece.getPossibleMoves(currentBoard))
 					{
-						System.out.println((currentDepth + 1) + ": " + currentPossibleMoveBoard.createFenString());
-						String test = currentPossibleMoveBoard.createFenString();
+						// System.out.println((currentDepth + 1) + ": " + currentPossibleMoveBoard.createFenString());
+
+						// Creates child node of "node"
 						BoardNode child = new BoardNode(node, null, 0, currentDepth + 1, currentPossibleMoveBoard, node.getAlpha(), node.getBeta());
 						node.addChild(child);
 
-						int score = maxAlpha(child);
+						// Obtains child score
+						int childScore = maxAlpha(child);
 
-						if (score <= node.getAlpha())
+						// If child score is less than node score
+						if (childScore < node.getHeuristic())
 						{
-							System.out.println(currentDepth + ": MIN PRUNED");
-							return node.getBeta();
-						}
-						if (score < node.getBeta())
-						{
-							node.setBeta(score);
+							node.setHeuristic(childScore);
 							node.setBestBoardNode(child);
+						}
+						// If child score is less than or equal to alpha
+						if (childScore <= node.getAlpha())
+						{
+							// System.out.println(currentDepth + ": MIN PRUNED");
+							return node.getHeuristic();
+						}
+						// If child score is less than beta
+						if (childScore < node.getBeta())
+						{
+							node.setBeta(childScore);
+							// System.out.println((currentDepth + ": NEW BETA: " + node.getBeta()));
 						}
 					}
 				}
 			}
 		}
 
-		if (node.getBestBoardNode() == null)
-		{
-			System.out.println(currentDepth + ": NULL NODE: " + node.getBoard().createFenString());
-		}
-		return node.getBeta();
+//		if (node.getBestBoardNode() == null)
+//		{
+//			System.out.println(currentDepth + ": NULL NODE: " + node.getBoard().createFenString());
+//		}
+
+		// Return best heuristic
+		return node.getHeuristic();
 	}
 
+	// Method will return the most maximized heuristic or return early if pruned
 	private int maxAlpha(BoardNode node)
 	{
 		int currentDepth = node.getDepth();
 		Board currentBoard = node.getBoard();
 		int maxDepth = this.restCallPackage.getDepth();
-		boolean isAIMoving = currentDepth % 2 == 0;
+		boolean isAIMoving = currentDepth % 2 == 0; // AI only moves on even depths
 		char enemyColor = this.getRestCallPackage().getAiColor() == 'b' ? 'w' : 'b';
 		char movingColor = isAIMoving ? this.getRestCallPackage().getAiColor() : enemyColor;
 
+		// If current depth of node has reached maximum, then terminal node has been reached
 		if (currentDepth == maxDepth)
 		{
 			// Terminal node reached, add score
@@ -147,6 +184,7 @@ public class ChessBot
 			return currentBoard.getAIScore();
 		}
 
+		// Counts how many pieces can be moved on the current player/AI's turn
 		int movingPieceCount = 0;
 		for (Pieces[] row : currentBoard.board)
 		{
@@ -158,6 +196,7 @@ public class ChessBot
 			}
 		}
 
+		// If there are zero pieces that can be moved in current turn then return as a terminal node
 		if (movingPieceCount == 0)
 		{
 			// Terminal node reached, add score
@@ -165,12 +204,18 @@ public class ChessBot
 			return currentBoard.getAIScore();
 		}
 
+		// Set initial value to negative infinity
+		node.setHeuristic(Integer.MIN_VALUE);
+
+		// Search all possible moves
 		for (Pieces[] row : currentBoard.board)
 		{
 			for (Pieces piece : row)
 			{
+				// If piece is an empty square, continue through loop
 				if (piece == null) continue;
 				int possibleMovesCount = piece.getPossibleMoves(currentBoard).size();
+				// If current piece has no moves, continue through loop
 				if (possibleMovesCount == 0)
 				{
 					// Terminal node reached, add score
@@ -179,37 +224,50 @@ public class ChessBot
 					continue;
 				}
 
+				// If piece == movingColor
 				if (piece.getIsWhite() == (movingColor == 'w'))
 				{
 					ArrayList<Board> possibleMoves = piece.getPossibleMoves(currentBoard);
 					for (Board currentPossibleMoveBoard : piece.getPossibleMoves(currentBoard))
 					{
-						System.out.println((currentDepth + 1) + ": " + currentPossibleMoveBoard.createFenString());
-						String test = currentPossibleMoveBoard.createFenString();
+						// System.out.println((currentDepth + 1) + ": " + currentPossibleMoveBoard.createFenString());
+
+						// Creates child node of "node"
 						BoardNode child = new BoardNode(node, null, 0, currentDepth + 1, currentPossibleMoveBoard, node.getAlpha(), node.getBeta());
 						node.addChild(child);
 
-						int score = minBeta(child);
+						// Obtains child score
+						int childScore = minBeta(child);
 
-						if (score >= node.getBeta())
+						// If child score is greater than node score
+						if (childScore > node.getHeuristic())
 						{
-							System.out.println(currentDepth + ": MAX PRUNED");
-							return node.getBeta();
-						}
-						if (score > node.getAlpha())
-						{
-							node.setAlpha(score);
+							node.setHeuristic(childScore);
 							node.setBestBoardNode(child);
+						}
+						// If child score is greater than or equal to beta
+						if (childScore >= node.getBeta())
+						{
+							// System.out.println(currentDepth + ": MAX PRUNED");
+							return node.getHeuristic();
+						}
+						// If child score is greater than alpha
+						if (childScore > node.getAlpha())
+						{
+							node.setAlpha(childScore);
+							// System.out.println((currentDepth + ": NEW ALPHA: " + node.getAlpha()));
 						}
 					}
 				}
 			}
 		}
 
-		if (node.getBestBoardNode() == null)
-		{
-			System.out.println(currentDepth + ": NULL NODE: " + node.getBoard().createFenString());
-		}
-		return node.getAlpha();
+//		if (node.getBestBoardNode() == null)
+//		{
+//			System.out.println(currentDepth + ": NULL NODE: " + node.getBoard().createFenString());
+//		}
+
+		// Return best heuristic
+		return node.getHeuristic();
 	}
 }
--- Board/src/test/ChessBotCLI.java
+++ Board/src/test/ChessBotCLI.java
@@ -8,12 +8,12 @@ public class ChessBotCLI
 {
 	public static void main (String[] args)
 	{
-		System.out.println("Please input Fen String");
+		System.out.println("ChessBot Test");
 		// Minimal branch test fenString: 1p6/P7/8/8/8/8/8/8
 		// Minimal branch test fenString for NPE Error Bug at DEPTH 4: 1p6/8/8/P7/8/8/8/8
+		// Minimal branch test for "only min-pruning" bug? at DEPTH 4: r3r3/8/8/2P5/8/8/8/8
 		// Default fenString: ppppkppp/pppppppp/8/8/8/8/PPPPPPPP/PPPPKPPP
-		RESTCallPackage restCallPackage = new RESTCallPackage("1p6/8/8/P7/8/8/8/8", 'b', 4, "white");
-		Board board = new Board(restCallPackage.getFenString(), null);
+		RESTCallPackage restCallPackage = new RESTCallPackage("ppppkppp/pppppppp/7P/8/8/8/PPPPPPPP/PPPPKPPP", 'b', 4, "white");
 		ChessBot theAI = new ChessBot(restCallPackage);
 		Board result = theAI.getBestMoveBoard();
 		System.out.println("This Board's Score is: " + result.getAIScore());
